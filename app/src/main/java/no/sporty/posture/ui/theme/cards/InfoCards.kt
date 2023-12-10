4c8680e301677a81f7942d18009b42a1379d1494
Ida Kristine Riseng <ida.kristine.riseng@posten.no>
2023-12-10 15:19:47 +0100

    Update InfoCards.kt

--- app/src/main/java/no/sporty/posture/ui/theme/cards/InfoCards.kt
+++ app/src/main/java/no/sporty/posture/ui/theme/cards/InfoCards.kt
@@ -9,15 +9,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Icon
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
