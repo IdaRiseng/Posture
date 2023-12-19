3401c71a175fd9beec4938a06ec568f49617fe80
Ida Kristine Riseng <ida.kristine.riseng@posten.no>
2023-12-19 20:29:28 +0100

    add movements and titles

--- app/src/main/java/no/sporty/posture/model/Exercise.kt
+++ app/src/main/java/no/sporty/posture/model/Exercise.kt
@@ -6,41 +6,83 @@ import no.sporty.posture.R
 enum class Exercise(
     val title: String,
     val shortDesc: String,
-    val longDesc: String,
     @DrawableRes val illustration: Int,
     val movements: List<Movement>
 ) {
     BUILD_AND_STRETCH(
         title = "Build and stretch",
-        shortDesc = "Strengthen your back muscles",
-        longDesc = "When having a bad posture, your front muscles is alot stronger than your back muscles. Stretch your front muscles and build your back muscle!",
+        shortDesc = "Strengthen your back muscles, stretch your front muscles",
         illustration = R.drawable.launcher_icon,
         movements = listOf(
-            Movement.CHEST_OPENER,
+            Movement.YWTS,
+            Movement.PRAYER_STRETCH,
+            Movement.SCARECROW,
+            Movement.SHOULDER_DISLOCATIONS,
             Movement.WALL_SLIDE,
-            Movement.TABLE_TOP_LIFT,
-            Movement.GLUTE_BRIDGE,
-            Movement.ALL_FOUR_TWIST
+            Movement.QUADRUPED_REACH,
+            Movement.DOOR_FRAME_STRETCH
         )
     ),
-    OFFICE_EXERCISE(
-        title = "Office exercise",
-        shortDesc = "Strengthen your back muscles",
-        longDesc = "When having a bad posture, your front muscles is alot stronger than your back muscles. Stretch your front muscles and build your back muscle!",
+    SIMPLE_OFFICE_EXERCISE(
+        title = "Simple office exercise",
+        shortDesc = "Exercises while sitting on a chair",
         illustration = R.drawable.launcher_icon,
         movements = listOf(
             Movement.CHEST_OPENER,
-            Movement.WALL_SLIDE,
+            Movement.NECK_STRETCH,
+            Movement.HEART_OPENER,
+            Movement.LOWER_BACK_STRETCH,
+            Movement.BACK_STRETCH,
+            Movement.TURTLE_NECK,
+            Movement.ANGEL_WINGS,
+            Movement.SHOULDER_CIRCLES
         )
     ),
-    LAYING_EXERCISE(
-        title = "Laying exercise",
-        shortDesc = "Strengthen your back muscles",
-        longDesc = "When having a bad posture, your front muscles is alot stronger than your back muscles. Stretch your front muscles and build your back muscle!",
+    MINDFUL_STRETCHING(
+        title = "Mindful stretching",
+        shortDesc = "Slow down and relax any tension, remember your breathing",
         illustration = R.drawable.launcher_icon,
         movements = listOf(
-            Movement.CHEST_OPENER,
-            Movement.WALL_SLIDE,
+            Movement.ANGEL_WINGS,
+            Movement.SHOULDER_CIRCLES,
+            Movement.NECK_STRETCH,
+            Movement.CAT_COW,
+            Movement.THORACIC_ROTATION,
+            Movement.EXTENDED_PUPPYPOSE,
+            Movement.LOTUS_POSE,
+            Movement.SHOULDER_STRETCH,
+            Movement.DYNAMIC_CAMEL,
+            Movement.TABLE_TOP_LIFT
+        )
+    ),
+    BED_EXERCISE(
+        title = "Bed exercises",
+        shortDesc = "Do exercises while laying in bed",
+        illustration = R.drawable.launcher_icon,
+        movements = listOf(
+            Movement.YWTS,
+            Movement.AB_STRETCH,
+            Movement.BACK_SQUEEZE_PULSE,
+            Movement.BACK_SWIM,
+            Movement.LOTUS_POSE,
+            Movement.SHOULDER_STRETCH
+        )
+    ),
+    AT_THE_GYM(
+        title = "At the gym",
+        shortDesc = "Use the tools at your gym for a better workout",
+        illustration = R.drawable.launcher_icon,
+        movements = listOf(
+            Movement.IDK,
+            Movement.IDK_TWO,
+            Movement.IDK_THREE,
+            Movement.SHOULDER_DISLOCATIONS_FOAM_ROLLER,
+            Movement.QUADRUPED_REACH,
+            Movement.SHOULDER_STRETCH,
+            Movement.PULL_APARTS,
+            Movement.PRAYER_STRETCH,
+            Movement.SCARECROW,
+            Movement.SHOULDER_DISLOCATIONS,
         )
     )
 }
--- app/src/main/java/no/sporty/posture/model/Movement.kt
+++ app/src/main/java/no/sporty/posture/model/Movement.kt
@@ -7,11 +7,46 @@ import no.sporty.posture.R
 
 
 enum class Movement(@StringRes val title: Int, @DrawableRes val illustration: Int, @RawRes val video: Int, info: MovementInfo?) {
-    CHEST_OPENER(R.string.chest_opener, R.drawable.launcher_icon, R.raw.test, null),
+
+    //Wall
     WALL_SLIDE(R.string.wall_slide, R.drawable.launcher_icon, R.raw.test, null),
+    WALL_TOUCH(R.string.wall_touch, R.drawable.launcher_icon, R.raw.test, null),
+    DOOR_FRAME_STRETCH(R.string.door_frame_stretch, R.drawable.launcher_icon, R.raw.test, null),
+
+    //Sitting
+    CHEST_OPENER(R.string.chest_opener, R.drawable.launcher_icon, R.raw.test, null),
+    NECK_STRETCH(R.string.neck_stretch, R.drawable.launcher_icon, R.raw.test, null),
+    HEART_OPENER(R.string.heart_opener, R.drawable.launcher_icon, R.raw.test, null),
+    LOWER_BACK_STRETCH(R.string.lower_back_stretch, R.drawable.launcher_icon, R.raw.test, null),
+    BACK_STRETCH(R.string.back_stretch, R.drawable.launcher_icon, R.raw.test, null),
+    TURTLE_NECK(R.string.turtle_neck, R.drawable.launcher_icon, R.raw.test, null),
+    ANGEL_WINGS(R.string.angel_wings, R.drawable.launcher_icon, R.raw.test, null),
+    SHOULDER_CIRCLES(R.string.shoulder_circles, R.drawable.launcher_icon, R.raw.test, null),
+
+    //Laying
     TABLE_TOP_LIFT(R.string.table_top_lift, R.drawable.launcher_icon, R.raw.test, null),
     GLUTE_BRIDGE(R.string.glute_bridge, R.drawable.launcher_icon, R.raw.test, null),
-    ALL_FOUR_TWIST(R.string.all_four_twist, R.drawable.launcher_icon, R.raw.test, null),
-    WALL_STRETCH(R.string.wall_stretch, R.drawable.launcher_icon, R.raw.test, null),
-    SQUAT(R.string.squat, R.drawable.launcher_icon, R.raw.test, null),
+    AB_STRETCH(R.string.ab_stretch, R.drawable.launcher_icon, R.raw.test, null),
+    DOWN_DOG_TWIST(R.string.down_dog_twist, R.drawable.launcher_icon, R.raw.test, null),
+    BACK_SQUEEZE_PULSE(R.string.back_squeeze_pulse, R.drawable.launcher_icon, R.raw.test, null),
+    BACK_SWIM(R.string.back_swim, R.drawable.launcher_icon, R.raw.test, null),
+    CAT_COW(R.string.cat_cow, R.drawable.launcher_icon, R.raw.test, null),
+    THORACIC_ROTATION(R.string.thoracic_rotation, R.drawable.launcher_icon, R.raw.test, null),
+    EXTENDED_PUPPYPOSE(R.string.extended_puppy_pose, R.drawable.launcher_icon, R.raw.test, null),
+    LOTUS_POSE(R.string.lotus_pose, R.drawable.launcher_icon, R.raw.test, null),
+    SHOULDER_STRETCH(R.string.shoulder_stretch, R.drawable.launcher_icon, R.raw.test, null),
+    DYNAMIC_CAMEL(R.string.dynamic_camel, R.drawable.launcher_icon, R.raw.test, null),
+    YWTS(R.string.y_w_t_s, R.drawable.launcher_icon, R.raw.test, null),
+
+
+    //Gym
+    IDK(R.string.idk, R.drawable.launcher_icon, R.raw.test, null),
+    IDK_TWO(R.string.idk_two, R.drawable.launcher_icon, R.raw.test, null),
+    IDK_THREE(R.string.idk_three, R.drawable.launcher_icon, R.raw.test, null),
+    SHOULDER_DISLOCATIONS_FOAM_ROLLER(R.string.shoulder_dislocation_foam_roller, R.drawable.launcher_icon, R.raw.test, null),
+    QUADRUPED_REACH(R.string.quadruped_reach, R.drawable.launcher_icon, R.raw.test, null),
+    PULL_APARTS(R.string.pull_aparts, R.drawable.launcher_icon, R.raw.test, null),
+    PRAYER_STRETCH(R.string.prayer_stretch, R.drawable.launcher_icon, R.raw.test, null),
+    SCARECROW(R.string.scarecrow, R.drawable.launcher_icon, R.raw.test, null),
+    SHOULDER_DISLOCATIONS(R.string.shoulder_dislocations, R.drawable.launcher_icon, R.raw.test, null),
 }
